a2eda3e2f34178ef3c677957ca690512785b9a4f
puppe1990 <matheus.puppe90@hotmail.com>
2025-07-20 00:07:51 -0300

    Rename project to "Algorithmic Art Generator" and update metadata in layout file for improved description and title.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Algorithmic Art Generator',
+  description: 'Create beautiful generative algorithmic art with customizable parameters',
+  generator: 'Algorithmic Art Generator',
 }
 
 export default function RootLayout({
